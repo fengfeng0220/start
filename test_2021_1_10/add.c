b116d2c739d21ec1780d10d82d77e16c402a4761
fengfeng0220 <18751969509@sina.cn>
2021-02-05 19:30:26 +0800

    first day

---
+++ test_2021_1_10/add.c
@@ -0,0 +1,19 @@
+//int g_val = 2021;
+
+//
+//
+//int main()
+//{
+//	//ื๗าต2
+//	return 0;
+//}
+
+#include<stdio.h>
+int main(void)
+{
+    int a = 40;
+    int c = 212;
+    int b = (-8 + 22) * a - 10 + c + 2;
+    printf("%d\n", b);
+    return 0;
+}
