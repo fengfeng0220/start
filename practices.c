4dd25177fd95a60d4f3b177061cf86dd3b774e60
fengfeng0220 <18751969509@sina.cn>
2021-02-06 15:22:52 +0800

    练习

---
+++ practices.c
@@ -0,0 +1,22 @@
+//请计算表达式“(-8+22)×a-10+c÷2”，其中，a = 40，c = 212。
+#include<stdio.h>
+
+int main()
+{
+	int a = 40;
+	int c = 212;
+	printf("%d\n", (-8 + 22)*a - 10 + c/2);
+
+	return 0;
+}
+//==>结果是656.
+ 
+//你的手机丢了，在屏幕上输出信息告诉大家。 
+#include<stdio.h>
+int main()
+{
+    printf("I lost my cellphone!");
+
+    return 0;
+}
+//==>结果是I lost my cellphone!
